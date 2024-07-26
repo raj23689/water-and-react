663c4f0c0dd39482be864443b31dc7117d39341d
raj23689 <92665411+raj23689@users.noreply.github.com>
2024-07-26 22:36:32 +0530

    Custom react mostly done.

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,1 +1,21 @@
-const mainContainer = document.querySelector('#app')
+const customRender = (reactElement, container) => {
+    const domEle = document.createElement(reactElement.type);
+    domEle.innerHTML = reactElement.children;
+    domEle.setAttribute('href', reactElement.props.href);
+    domEle.setAttribute('target', reactElement.props.target);
+
+    container.appendChild(domEle);
+};
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://searx.garudalinux.org',
+        target: '_blank',
+    },
+    children: 'Click me to visit searxng'
+};
+
+const mainContainer = document.querySelector('#app');
+
+customRender(reactElement, mainContainer);
