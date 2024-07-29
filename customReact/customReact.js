142fd9a1c5155b4f3fba342610f4a85a5dff412c
raj23689 <92665411+raj23689@users.noreply.github.com>
2024-07-29 21:57:15 +0530

    Custom react v2.

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,9 +1,19 @@
 const customRender = (reactElement, container) => {
+   /*
     const domEle = document.createElement(reactElement.type);
     domEle.innerHTML = reactElement.children;
     domEle.setAttribute('href', reactElement.props.href);
     domEle.setAttribute('target', reactElement.props.target);
 
+    container.appendChild(domEle);
+    */
+
+    const domEle = document.createElement(reactElement.type)
+    domEle.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domEle.setAttribute(prop, reactElement.props[prop]);
+    }
     container.appendChild(domEle);
 };
 
